7b8b9f81bf60e637683b9efd00c03bb8310ccd84
Mokobake <67834518+Mokobake@users.noreply.github.com>
2023-03-18 19:53:27 +0900

    Add files via upload

--- TPSShootingGame Cheat/main.cpp
+++ TPSShootingGame Cheat/main.cpp
@@ -7,6 +7,7 @@
 DWORD PID;
 HANDLE ProcessHandle;
 uintptr_t BaseAddress;
+bool fly;
 
 int main()
 {
@@ -37,7 +38,9 @@ int main()
 		uintptr_t PlayerController = read<uintptr_t>(ProcessHandle, LocalPlayer + 0x30);
 		uintptr_t PlayerCameraManager = read<uintptr_t>(ProcessHandle, PlayerController + 0x2b8);
 		uintptr_t Pawn = read<uintptr_t>(ProcessHandle, PlayerController + 0x250);
-		
+		DWORD_PTR RootComponent = read<DWORD_PTR>(ProcessHandle, Pawn + 0x130);
+		Vector3 PawnRelativeLocation = read<Vector3>(ProcessHandle, RootComponent + 0x11c);
+
 		if (GetAsyncKeyState(VK_LSHIFT))
 		{
 			write<float>(ProcessHandle, Pawn + 0x98, 2);
@@ -48,5 +51,50 @@ int main()
 		}
 
 		write<float>(ProcessHandle, PlayerCameraManager + 0x238 + 0x4, 130);
+
+		if (GetAsyncKeyState(VK_NUMPAD1) & 1)
+		{
+			fly = !fly;
+
+			if (fly)
+			{
+				Beep(500, 500);
+			}
+			else
+			{
+				Beep(300, 500);
+			}
+		}
+
+		if (fly)
+		{
+			if (GetAsyncKeyState('W'))
+			{
+				PawnRelativeLocation.x += 0.1;
+			}
+			if (GetAsyncKeyState('S'))
+			{
+				PawnRelativeLocation.x -= 0.1;
+			}
+			if (GetAsyncKeyState('A'))
+			{
+				PawnRelativeLocation.y -= 0.1;
+			}
+			if (GetAsyncKeyState('D'))
+			{
+				PawnRelativeLocation.y += 0.1;
+			}
+			if (GetAsyncKeyState(VK_SPACE))
+			{
+				PawnRelativeLocation.z += 0.1;
+			}
+			if (GetAsyncKeyState(VK_LSHIFT))
+			{
+				PawnRelativeLocation.z -= 0.1;
+			}
+
+			write<Vector3>(ProcessHandle, RootComponent + 0x11c, PawnRelativeLocation);
+			write<char>(ProcessHandle, RootComponent + 0x14c, 0);
+		}
 	}
 }
--- TPSShootingGame Cheat/utils.hpp
+++ TPSShootingGame Cheat/utils.hpp
@@ -61,3 +61,54 @@ void write(HANDLE ProcessHandle, uintptr_t Address, T Buffer)
 {
 	WriteProcessMemory(ProcessHandle, LPVOID(Address), &Buffer, sizeof(Buffer), 0);
 }
+
+class Vector3
+{
+public:
+	Vector3() : x(0.f), y(0.f), z(0.f)
+	{
+
+	}
+
+	Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z)
+	{
+
+	}
+	~Vector3()
+	{
+
+	}
+
+	float x;
+	float y;
+	float z;
+
+	inline float Dot(Vector3 v)
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+
+	inline float Distance(Vector3 v)
+	{
+		return float(sqrtf(powf(v.x - x, 2.0) + powf(v.y - y, 2.0) + powf(v.z - z, 2.0)));
+	}
+
+	Vector3 operator+(Vector3 v)
+	{
+		return Vector3(x + v.x, y + v.y, z + v.z);
+	}
+
+	Vector3 operator-(Vector3 v)
+	{
+		return Vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	Vector3 operator*(float number) const {
+		return Vector3(x * number, y * number, z * number);
+	}
+
+	float Length()
+	{
+		return sqrt((x * x) + (y * y) + (z * z));
+	}
+};
